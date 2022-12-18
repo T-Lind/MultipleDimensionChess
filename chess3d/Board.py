fe9521f198510f72ec0c0bc2b20db61b16d0a6fd
T-Lind <tiernanxklindauer@gmail.com>
2022-12-17 22:54:23 -0500

    getting close, still need to make sure all moves are valid and actually display the graphics

--- chess3d/Board.py
+++ chess3d/Board.py
@@ -1,7 +1,9 @@
 import numpy as np
+from math import hypot
 
 from chess3d.Pieces import Piece
 from chess3d.Exceptions import MovementException
+
 WHITE = Piece.WHITE
 BLACK = Piece.BLACK
 
@@ -11,7 +13,7 @@ class Board:
     def __init__(self):
         self.array = np.full((8, 8, 8), Piece.NONE, dtype=Piece)
 
-        self.captured_material = {"w":0, "b":0}
+        self.captured_material = {"w": 0, "b": 0}
 
         # Fill with pawns
         for i in range(8):
@@ -47,9 +49,10 @@ class Board:
                 print(item.string, end=" ")
             print()
 
-    def set(self, x, y, z, piece):
+    def set(self, x, y, z, piece) -> None:
         self.array[x][y][z] = piece
-    def get(self, x, y, z):
+
+    def get(self, x, y, z) -> Piece:
         return self.array[x][y][z]
 
     def print_cube(self) -> None:
@@ -68,9 +71,35 @@ class Board:
                 if self.check_filled(Piece.opposite(player), *to):
                     # Keep track of material score when capturing
                     self.captured_material[player.type] += self.get(*to).material
-                # Move the piece from `pos` to `to`
-                self.set(*to, self.get(*pos))
+                if self.is_legal_movement(pos, to, self.get(*pos)):
+                    # Move the piece from `pos` to `to`
+                    self.set(*to, self.get(*pos))
             else:
                 raise MovementException("Location selected to move is filled with the same piece color")
         else:
             raise MovementException("Piece selected to move was not of correct type")
+
+    def is_legal_movement(self, pos, to, piece) -> bool:
+        # TODO: Finish this legal movement code, need to do for pawns, bishops, knights, and queens
+        pos_set = set(pos)
+        to_set = set(to)
+        # Coordinates should not be the same. TODO: Check and see if this is valid in Python, can't remember
+        if pos == to:
+            return False
+        # Coordinates should not be out of bounds
+        if min(pos_set.union(to_set)) < 0 or max(pos_set.union(to_set)) > 7:
+            return False
+
+        # Rook case
+        if piece.string[1] == "R":
+            # Final position should have two coordinates in common
+            return len(to_set.intersection(pos_set)) == 2
+
+        # King case
+        if piece.string[1] == "K":
+            # Final position should be one difference in any direction, including diagonals
+            # TODO: check if I'm high when writing this, this should calculate the 3d hypotenuse
+            hypot1 = hypot(to[0] - pos[0], to[1] - pos[1])
+            hypot2 = hypot(to[0] - pos[0], to[2] - pos[2])
+            distance = hypot(hypot1, hypot2)
+            return distance < 2
