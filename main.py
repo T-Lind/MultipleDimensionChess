7b19244398780d71418904afc6e28000aa27e798
T-Lind <tiernanxklindauer@gmail.com>
2022-12-19 17:33:25 -0600

    king and queen drawn as well - all pieces drawn properly now

--- main.py
+++ main.py
@@ -37,21 +37,39 @@ def draw_bishop(x, y, z, color_str):
 
 def draw_pawn(x, y, z, color_str):
     x, y, z = reorient_offset(x, y, z)
-    # sphere(pos=vector(x, y, z), size=vector(1, 1.5, 1), color=full_color[color_str])
     cylinder(pos=vector(x, y, z), size=vector(1, 0.75, 1), axis=vector(0, 1, 0), color=full_color[color_str])
 
+def draw_rook(x, y, z, color_str):
+    x, y, z = reorient_offset(x, y, z)
 
+    cylinder(pos=vector(x, y, z), size=vector(0.5, 1.5, 1.5), axis=vector(0, 1, 0), color=full_color[color_str])
+    cylinder(pos=vector(x, y-0.5, z), size=vector(1, 0.75, 0.75), axis=vector(0, 1, 0), color=full_color[color_str])
+
+def draw_queen(x, y, z, color_str):
+    x, y, z = reorient_offset(x, y, z)
+
+    cylinder(pos=vector(x, y - 0.7, z), size=vector(0.5, 1.5, 1.5), axis=vector(0, 1, 0), color=full_color[color_str])
+    sphere(pos=vector(x, y + 0.1, z), size=vector(1.5, 0.75, 1), axis=vector(0, 1, 0), color=full_color[color_str])
+    cylinder(pos=vector(x, y+0.3, z), size=vector(0.1, 1.5, 1.5), axis=vector(0, 1, 0), color=full_color[color_str])
+    cylinder(pos=vector(x, y+0.5, z), size=vector(0.1, 1.5, 1.5), axis=vector(0, 1, 0), color=color.yellow)
+
+def draw_king(x, y, z, color_str):
+    x, y, z = reorient_offset(x, y, z)
+
+    cylinder(pos=vector(x, y - 0.7, z), size=vector(0.5, 1.5, 1.5), axis=vector(0, 1, 0), color=full_color[color_str])
+    box(pos=vector(x, y+0.1, z), size=vector(0.3, 1.5, 0.3), color=color.yellow)
+    box(pos=vector(x, y+0.5, z), size=vector(0.3, 0.3, 0.75), color=color.yellow)
 
 if __name__ == '__main__':
     draw_board()
-    # Knight
-    draw_knight(0, 0, 0, "w")
+
+    draw_king(0, 0, 0, "w")
 
     # +X
-    draw_knight(7, 0, 0, "w")
+    draw_rook(7, 0, 0, "w")
 
     # +Y
-    draw_bishop(0, 6, 0, "w")
+    draw_queen(0, 6, 0, "w")
 
     # +Z
     draw_bishop(0, 0, 7, "w")
